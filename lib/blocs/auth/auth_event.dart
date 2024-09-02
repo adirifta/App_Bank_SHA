8434f8f1cea1f8bd22af9584071865ba8c5a5018
adirifta <adiriftadk@gmail.com>
2024-09-02 14:01:32 +0700

    Membuat API TopUp

--- lib/blocs/auth/auth_event.dart
+++ lib/blocs/auth/auth_event.dart
@@ -54,6 +54,12 @@ class AuthUpdatePin extends AuthEvent {
   List<Object> get props => [oldPin, newPin];
 }
 
-class Authlogout extends AuthEvent{
-  
+class Authlogout extends AuthEvent {}
+
+class AuthUpdateBalance extends AuthEvent {
+  final int amount;
+  const AuthUpdateBalance(this.amount);
+
+   @override
+  List<Object> get props => [amount];
 }
